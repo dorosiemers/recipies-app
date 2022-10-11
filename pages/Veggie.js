88be7576b612bce526cafed069e8ae09f6c6228a
Doro Siemers <dorotheasiemers@gmail.com>
2022-10-11 22:54:53 +0200

    Challenge 3 completed

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import VeggiePizza from "../public/VeggiePizza.jpg";
 
 export default function Veggie() {
   return (
     <div>
       <h1>Vegetarische Wunderwelten</h1>
       <p>Iss mehr Bohnen!</p>
+      <Image src={VeggiePizza} alt="Veggie Pizza" layout="responsive" />
     </div>
   );
 }
